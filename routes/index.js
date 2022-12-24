efc79db21af7077a5fc2cede31228a56106e7bba
tim_coder <itteamdev74@gmail.com>
2022-12-24 17:54:44 +0500

    routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const Homes = require("../model/Homes")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/addNewPost', async (req, res) => {
+  const home = await new Homes(req.body)
+  await home.save();
+  res.json(home)
+})
 
 module.exports = router;
